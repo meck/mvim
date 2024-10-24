ff66189a87ac2e1a7303bb2fbdf2b750166e1d00
meck <Johan@meck.se>
2024-10-24 09:12:06 +0200

    lualine: Update cfg

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -1,75 +1,95 @@
 {
   plugins.lualine = {
     enable = true;
-    settings = {
-      globalstatus = true;
-      alwaysDivideMiddle = true;
-      extensions = [ "quickfix" ];
-      sectionSeparators = {
-        left = "";
-        right = "";
-      };
-      componentSeparators = {
-        left = "";
-        right = "";
-      };
-      sections = {
-        lualine_a = [
-          "mode"
-          {
-            __unkeyed-1.__raw =
-              # lua
-              ''
-                function()
-                  local km = vim.bo.keymap
-                  if km ~= "" then
-                      km = "  " .. km
-                  end
-                  return km
-                end
-              '';
-          }
+    settings =
 
-          {
-            __unkeyed-1.__raw =
-              # lua
-              ''
-                function()
-                    if vim.fn["g:tablemode#IsActive"]() == 1 then
-                        return " "
+      let
+        filenameSec = {
+          __unkeyed-1 = "filename";
+          symbols = {
+            modified = " ";
+          };
+        };
+        diffSec = {
+          __unkeyed-1 = "diff";
+          symbols = {
+            added = " ";
+            modified = " ";
+            removed = " ";
+          };
+        };
+      in
+      {
+        options = {
+          alwaysDivideMiddle = true;
+          extensions = [ "quickfix" ];
+          component_separators = {
+            left = "";
+            right = "";
+          };
+          section_separators = {
+            left = "";
+            right = "";
+          };
+        };
+
+        sections = {
+          lualine_a = [
+            "mode"
+            {
+              __unkeyed-1.__raw =
+                # lua
+                ''
+                  function()
+                    local km = vim.bo.keymap
+                    if km ~= "" then
+                        km = "  " .. km
                     end
-                    return ""
-                end
-              '';
-          }
-        ];
-        lualine_b = [
-          "branch"
-          "diff"
-        ];
-        lualine_c = [
-          "filename"
-          "navic"
-        ];
-        lualine_x = [
-          "spaces"
-          "encoding"
-          "filetype"
-        ];
-        lualine_y = [ "diagnostics" ];
-        lualine_z = [
-          "progress"
-          "location"
-        ];
+                    return km
+                  end
+                '';
+            }
+
+            {
+              __unkeyed-1.__raw =
+                # lua
+                ''
+                  function()
+                      if vim.fn["g:tablemode#IsActive"]() == 1 then
+                          return " "
+                      end
+                      return ""
+                  end
+                '';
+            }
+          ];
+          lualine_b = [
+            "branch"
+            diffSec
+          ];
+          lualine_c = [
+            filenameSec
+            "navic"
+          ];
+          lualine_x = [
+            "spaces"
+            "encoding"
+            "filetype"
+          ];
+          lualine_y = [ "diagnostics" ];
+          lualine_z = [
+            "progress"
+            "location"
+          ];
+        };
+        inactive_sections = {
+          lualine_b = [
+            "branch"
+            diffSec
+          ];
+          lualine_c = [ filenameSec ];
+        };
       };
-      inactiveSections = {
-        lualine_b = [
-          "branch"
-          "diff"
-        ];
-        lualine_c = [ "filename" ];
-      };
-    };
   };
 
   plugins.navic = {
