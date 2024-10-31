e8f0af9cc27d5fdad8e5aca5b7a1ce3d6e45bf86
meck <Johan@meck.se>
2024-10-31 21:37:51 +0100

    Update cmp cfg

--- config/plugins/completion/default.nix
+++ config/plugins/completion/default.nix
@@ -1,10 +1,5 @@
 _: {
   plugins = {
-    lspkind = {
-      enable = true;
-      cmp.enable = true;
-    };
-
     luasnip = {
       enable = true;
       fromLua = [ { paths = ./snippets; } ];
@@ -12,16 +7,34 @@ _: {
 
     friendly-snippets.enable = true;
 
+    lspkind = {
+      enable = true;
+      cmp = {
+        enable = true;
+        menu = {
+          nvim_lsp = "";
+          treesitter = "";
+          luasnip = "";
+          nvim_lua = "";
+          conventionalcommits = "󰊢";
+          path = "";
+          spell = "󰓆";
+          buffer = "";
+        };
+      };
+    };
+
     cmp = {
       enable = true;
+      luaConfig.pre = "luasnip = require('luasnip')";
       settings = {
-        snippet.expand = "luasnip";
         sources = [
           { name = "nvim_lsp"; }
-          { name = "nvim_lua"; }
           { name = "luasnip"; }
           { name = "treesitter"; }
+          { name = "nvim_lua"; }
           { name = "buffer"; }
+          { name = "conventionalcommits"; }
           {
             name = "path";
             groupIndex = 1;
@@ -33,84 +46,88 @@ _: {
         ];
 
         mapping = {
-          "<CR>" =
-            # lua
-            ''cmp.mapping.confirm({ select = true })'';
-          "<C-d>" =
-            # lua
-            ''cmp.mapping.scroll_docs(-4)'';
-          "<C-f>" =
-            # lua
-            ''cmp.mapping.scroll_docs(4)'';
-          "<C-Space>" =
-            # lua
-            ''cmp.mapping.complete()'';
-          "<C-e>" =
-            # lua
-            ''cmp.mapping.abort()'';
-          "<Tab>" =
-            # lua
+          "<CR>" = # lua
             ''
               cmp.mapping(function(fallback)
-                local has_words_before = function()
-                  unpack = unpack or table.unpack
-                  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+                if cmp.visible() then
+                  if luasnip.expandable() then
+                    luasnip.expand()
+                  else
+                    cmp.confirm({
+                      select = true,
+                    })
+                  end
+                else
+                  fallback()
                 end
-
+              end, {"i", "s", "c"})
+            '';
+          "<Esc>" = # lua
+            ''
+              cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.abort()
+                else
+                  fallback()
+                end
+              end, {'i', 'c'})
+            '';
+          "<Tab>" = # lua
+            ''
+              cmp.mapping(function(fallback)
                 if cmp.visible() then
                   cmp.select_next_item()
-                elseif require('luasnip').expand_or_jumpable() then
-                  require('luasnip').expand_or_jump()
-                elseif has_words_before() then
-                  cmp.complete()
+                elseif luasnip.locally_jumpable(1) then
+                  luasnip.jump(1)
                 else
                   fallback()
                 end
               end, { "i", "s" })
             '';
-          "<S-Tab>" =
-            # lua
+          "<S-Tab>" = # lua
             ''
               cmp.mapping(function(fallback)
                 if cmp.visible() then
                   cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
+                elseif luasnip.locally_jumpable(-1) then
                   luasnip.jump(-1)
                 else
                   fallback()
                 end
-              end, { "i", "s"})
+              end, { "i", "s" })
             '';
+          "<C-Space>" = # lua
+            ''cmp.mapping.complete()'';
+          "<C-d>" = # lua
+            ''cmp.mapping.scroll_docs(-4)'';
+          "<C-f>" = # lua
+            ''cmp.mapping.scroll_docs(4)'';
+        };
+
+        snippet.expand = # lua
+          ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
+            end
+          '';
+      };
+
+      cmdline = {
+        "/" = {
+          mapping.__raw = "cmp.mapping.preset.cmdline()";
+          formatting.fields = [ "abbr" ];
+          sources = [ { name = "buffer"; } ];
+        };
+        ":" = {
+          mapping.__raw = "cmp.mapping.preset.cmdline()";
+          formatting.fields = [ "abbr" ];
+          sources = [
+            { name = "path"; }
+            { name = "cmdline"; }
+          ];
+          matching.disallow_prefix_unmatching = false;
         };
       };
     };
-    cmp-cmdline.enable = true;
   };
-  extraConfigLua = ''
-    local cmp = require('cmp')
-
-    cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(),
-      formatting = {
-        fields = { "abbr" },
-      },
-      sources = {
-        { name = 'buffer' }
-      },
-    })
-
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      formatting = {
-        fields = { "abbr" },
-      },
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        { name = 'cmdline' }
-      })
-    })
-
-  '';
 }
