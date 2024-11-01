eea809a2cb921dec0e8cbd18301b20cc59b481f6
meck <Johan@meck.se>
2024-11-01 13:34:13 +0100

    Disable nixd semantic tokens again

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -85,6 +85,11 @@ in
           diagnostic.suppress = [ "sema-escaping-with" ];
           formatting.command = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
         };
+        # Dosent work with the `# lua` in nixvim
+        onAttach.function = # lua
+          ''
+            client.server_capabilities.semanticTokensProvider = nil
+          '';
       };
     };
 
