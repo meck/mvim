0e700678169ebf22c93cb8ecebd29997a8bd91be
meck <Johan@meck.se>
2024-11-03 12:03:02 +0100

    markdown: add preview

--- config/plugins/markdown.nix
+++ config/plugins/markdown.nix
@@ -1,6 +1,21 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (config.mvim) small;
+in
 {
   plugins.render-markdown.enable = true;
+  plugins.markdown-preview = {
+    enable = !small;
+    settings = {
+      auto_close = 0;
+      combine_preview = 1;
+    };
+  };
   extraPackages = [ pkgs.python312Packages.pylatexenc ];
 
   plugins.which-key.settings.spec = [
@@ -11,25 +26,38 @@
     }
   ];
 
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>mm";
-      action.__raw = "require('render-markdown').toggle";
-      options = {
-        silent = true;
-        desc = "MD: Toggle render-markdown";
-      };
-    }
-    {
-      mode = "v";
-      key = "<leader>mt";
-      action = ":! tr -s ' ' | column -t -s '|' -o '|'<CR>";
-      options = {
-        silent = true;
-        desc = "MD: Format selected table";
-      };
-    }
-  ];
+  keymaps =
+    [
+      {
+        mode = "n";
+        key = "<leader>mm";
+        action.__raw = "require('render-markdown').toggle";
+        options = {
+          silent = true;
+          desc = "MD: Toggle render-markdown";
+        };
+      }
+      {
+        mode = "v";
+        key = "<leader>mt";
+        action = ":! tr -s ' ' | column -t -s '|' -o '|'<CR>";
+        options = {
+          silent = true;
+          desc = "MD: Format selected table";
+        };
+      }
+    ]
+    ++ lib.optionals (!small) [
+
+      {
+        mode = "n";
+        key = "<leader>mp";
+        action = "<Plug>MarkdownPreviewToggle";
+        options = {
+          silent = true;
+          desc = "MD: Toggle browser preview";
+        };
+      }
+    ];
 
 }
