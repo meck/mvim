88b73e802b8da6ec1a27f7f5c61010a28adf0aed
meck <Johan@meck.se>
2025-01-22 10:29:03 +0100

    fix: overlays aren't applied on darwin

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -3,12 +3,18 @@
   imports = [ nixvim.homeManagerModules.nixvim ];
   config = {
     programs.nixvim =
-      { pkgs, lib, ... }:
-      (import ../config { inherit pkgs lib; })
+      { lib, ... }:
+      let
+        standaloneCfg = (import ../config { inherit lib; });
+      in
+      standaloneCfg
       // {
         config = {
           enable = true;
           viAlias = true;
+          # BUG: overlays dont get applied on darwin
+          # unless we do this
+          inherit (standaloneCfg.config) nixpkgs;
         };
       };
   };
