96353462db3344fca5bef46ad3d2e958167ff9f4
meck <Johan@meck.se>
2025-01-30 09:38:05 +0100

    ltex: switch to ltex-plus

--- config/plugins/lsp/ltex.nix
+++ config/plugins/lsp/ltex.nix
@@ -1,8 +1,14 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 lib.mkIf (!config.mvim.small) {
   plugins = {
-    lsp.servers.ltex = {
+    lsp.servers.ltex_plus = {
       enable = true;
+      package = pkgs.ltex-ls-plus;
       # https://github.com/nix-community/nixvim/issues/2361
       filetypes = [
         "gitcommit"
@@ -34,6 +40,21 @@ lib.mkIf (!config.mvim.small) {
           ''vim.fn.expand("~") .. "/.local/share/ltex"'';
       };
     };
+
+    # TODO: https://github.com/barreiroleo/ltex_extra.nvim/pull/66
+    # Plus new nixvim config needed to load with ltex-ls-plus
+    # ltex-extra = {
+    #   enable = true;
+    #   settings = {
+    #     load_langs = [
+    #       "en-US"
+    #       "sv"
+    #     ];
+    #     path.__raw =
+    #       # lua
+    #       ''vim.fn.expand("~") .. "/.local/share/ltex"'';
+    #   };
+    # };
   };
 
   keymaps = [
@@ -42,7 +63,7 @@ lib.mkIf (!config.mvim.small) {
       key = "<leader>lt";
       action.__raw = ''
         function() 
-          local server_name = "ltex"
+          local server_name = "ltex_plus"
           local bufnr = vim.api.nvim_get_current_buf()
           local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
           for _, client in ipairs(clients) do
