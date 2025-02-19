736b35eee70b4f5ff917044a479dd4983942e4e9
meck <Johan@meck.se>
2025-02-19 09:31:33 +0100

    fix: ltex

--- config/plugins/lsp/ltex.nix
+++ config/plugins/lsp/ltex.nix
@@ -6,9 +6,10 @@
 }:
 lib.mkIf (!config.mvim.small) {
   plugins = {
-    lsp.servers.ltex_plus = {
+    lsp.servers.ltex = {
       enable = true;
       package = pkgs.ltex-ls-plus;
+      cmd = [ "ltex-ls-plus" ];
       # https://github.com/nix-community/nixvim/issues/2361
       filetypes = [
         "gitcommit"
@@ -40,21 +41,6 @@ lib.mkIf (!config.mvim.small) {
           ''vim.fn.expand("~") .. "/.local/share/ltex"'';
       };
     };
-
-    # TODO: https://github.com/barreiroleo/ltex_extra.nvim/pull/66
-    # Plus new nixvim config needed to load with ltex-ls-plus
-    # ltex-extra = {
-    #   enable = true;
-    #   settings = {
-    #     load_langs = [
-    #       "en-US"
-    #       "sv"
-    #     ];
-    #     path.__raw =
-    #       # lua
-    #       ''vim.fn.expand("~") .. "/.local/share/ltex"'';
-    #   };
-    # };
   };
 
   keymaps = [
