79d7baab290495b7e7ef52efb630a9811609cee0
meck <Johan@meck.se>
2025-04-10 15:42:07 +0200

    update oil cfg

--- config/plugins/oil.nix
+++ config/plugins/oil.nix
@@ -2,16 +2,23 @@
   plugins.oil = {
     enable = true;
     settings = {
+      skip_confirm_for_simple_edits = true;
+      watch_for_changes = true;
+
       columns = [
-        "icons"
         "permissions"
         "size"
+        "icon"
       ];
+
       keymaps = {
         "<C-c>" = false;
+        "q" = "actions.close";
         "<C-l>" = false;
         "<C-r>" = "actions.refresh";
-        "q" = "actions.close";
+        "<C-s>" = false; # open vert
+        "<C-h>" = false; # open hor
+        "<C-t>" = false; # open tab
         "y." = "actions.copy_entry_path";
       };
     };
