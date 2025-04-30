112b72e21048c2875f027e560b6b5f9fc6f49c91
meck <Johan@meck.se>
2025-04-30 20:48:48 +0200

    oelint remove overlay

--- config/default.nix
+++ config/default.nix
@@ -10,20 +10,6 @@
   config = {
     nixpkgs.overlays = [
       (final: prev: {
-        oelint-adv = prev.oelint-adv.overridePythonAttrs (oldAttrs: {
-          # Fails on aarch64-darwin
-          doCheck = false;
-        });
-        # TODO wait for https://github.com/c0fec0de/anytree/issues/270
-        # TODO wait for https://github.com/NixOS/nixpkgs/issues/375763
-        python3Packages = prev.python3Packages.overrideScope (
-          _finalPy: prevPy: {
-            anytree = prevPy.anytree.overrideAttrs (old: {
-              patches = old.patches ++ [ ./python-anytree-poetry-project-name-version.patch ];
-            });
-          }
-        );
-
       })
     ];
   };
