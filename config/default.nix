0123710ff3cafd95e6292a43e4ddb924ed6dcb2e
meck <Johan@meck.se>
2025-07-22 20:24:34 +0200

    fix: mac check issue

--- config/default.nix
+++ config/default.nix
@@ -10,6 +10,16 @@
   config = {
     nixpkgs.overlays = [
       (final: prev: {
+        # https://github.com/nix-community/nixvim/issues/3576
+        vimPlugins = prev.vimPlugins // {
+          CopilotChat-nvim = prev.vimPlugins.CopilotChat-nvim.overrideAttrs (_: {
+            nvimSkipModules = [ "CopilotChat.integrations.fzflua" ];
+          });
+          nvim-notify = prev.vimPlugins.nvim-notify.overrideAttrs (_: {
+            nvimSkipModules = [ "notify.integrations.fzf" ];
+          });
+
+        };
       })
     ];
   };
