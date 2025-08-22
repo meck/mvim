0295a588dc656dba0fe3579a5abda059a7136b19
meck <Johan@meck.se>
2025-08-22 20:58:37 +0200

    Remove upstream fixes

--- config/default.nix
+++ config/default.nix
@@ -10,28 +10,7 @@
   config = {
     nixpkgs.overlays = [
       (final: prev: {
-        # https://github.com/nix-community/nixvim/issues/3576
         vimPlugins = prev.vimPlugins // {
-          CopilotChat-nvim = prev.vimPlugins.CopilotChat-nvim.overrideAttrs (_: {
-
-            src =
-              let
-                oldVersionDate = prev.vimPlugins.CopilotChat-nvim.version;
-                overlayVersionDate = "2025-08-10";
-                oldVersionInt = lib.toInt (builtins.replaceStrings [ "-" ] [ "" ] oldVersionDate);
-                overlayVersionInt = lib.toInt (builtins.replaceStrings [ "-" ] [ "" ] overlayVersionDate);
-              in
-              assert lib.assertMsg (overlayVersionInt > oldVersionInt) "Remove copilot chat version overlay";
-
-              final.fetchFromGitHub {
-                owner = "CopilotC-Nvim";
-                repo = "CopilotChat.nvim";
-                rev = "f5fd1a7ead5ccdd240fc3ef6e740fb49f74a1294";
-                hash = "sha256-bTMGu85jlgYyRLh1ugRxzLCrEznjIunhVf3XJpDMfEw=";
-              };
-
-            nvimSkipModules = [ "CopilotChat.integrations.fzflua" ];
-          });
         };
       })
     ];
