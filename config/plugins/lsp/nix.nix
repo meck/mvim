b239a91ddedf8cdd7aa0b02bd1a25260af2208d8
meck <Johan@meck.se>
2025-11-08 22:38:26 +0100

    nixd: remove unneeded config
    sema.. and semantic tokens is disabled by default now

--- config/plugins/lsp/nix.nix
+++ config/plugins/lsp/nix.nix
@@ -3,13 +3,7 @@
   plugins.lsp.servers.nixd = {
     enable = true;
     settings = {
-      diagnostic.suppress = [ "sema-escaping-with" ];
-      formatting.command = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
+      formatting.command = [ "${pkgs.nixfmt}/bin/nixfmt" ];
     };
-    # Dosent work with the `# lua` in nixvim
-    onAttach.function = # lua
-      ''
-        client.server_capabilities.semanticTokensProvider = nil
-      '';
   };
 }
