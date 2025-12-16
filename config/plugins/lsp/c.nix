b26ccd7c6f0398dd52b4331595cfb9d490f07afb
meck <Johan@meck.se>
2025-12-16 18:11:08 +0100

    Fix clangd for cross toolchains

--- config/plugins/lsp/c.nix
+++ config/plugins/lsp/c.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 {
@@ -14,25 +15,40 @@
 
   plugins.lsp = {
     servers = {
-      clangd = {
-        enable = true;
-        # Defaults includes .proto
-        filetypes = [
-          "c"
-          "cpp"
-          "objc"
-          "objcpp"
-          "cuda"
-        ];
-        package = lib.mkIf config.mvim.small null;
-        cmd = [
-          "clangd"
-          "--background-index"
-          "--clang-tidy"
-          "--fallback-style=llvm"
-          "--query-driver=/**/*"
-        ];
-      };
+      clangd =
+        let
+          # https://github.com/coletrammer/dotfiles/blob/4c6d15499037db9e165eaa2e7f1d44fe2065ea6c/home/nvim/lang/cpp.nix#L62
+          # Seeing issues when using cross toolchains
+          clang = pkgs.llvmPackages.libcxxClang;
+          clangUnwrapped = pkgs.llvmPackages.clang-unwrapped;
+          clangd = pkgs.writeShellScriptBin "clangd" ''
+            export CPATH=''${CPATH}''${CPATH:+':'}:${clang}/resource-root/include
+            export CPLUS_INCLUDE_PATH=''${CPLUS_INCLUDE_PATH}''${CPLUS_INCLUDE_PATH:+':'}:${clang}/resource-root/include
+            exec -a "$0" ${clangUnwrapped}/bin/$(basename $0) "$@"
+          '';
+        in
+        {
+          enable = true;
+          # Defaults includes .proto
+          filetypes = [
+            "c"
+            "cpp"
+            "objc"
+            "objcpp"
+            "cuda"
+          ];
+          package = lib.mkMerge [
+            (lib.mkIf (!config.mvim.small) clangd)
+            (lib.mkIf (config.mvim.small) null)
+          ];
+          cmd = [
+            "clangd"
+            "--background-index"
+            "--clang-tidy"
+            "--fallback-style=llvm"
+            "--query-driver=/**/*"
+          ];
+        };
     };
   };
 
